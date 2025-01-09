019ab56a4c6e4f2765e66642062a84d32bebf966
Akash Ajagekar <57575159+akashajagekar54@users.noreply.github.com>
2025-01-09 09:27:30 -0500

    Delete trait-extraction directory

--- trait-extraction/demo_crop_geometry.py
+++
@@ -1,8 +0,0 @@
-import open3d as o3d
-
-def demo_crop_geometry(point_cloud):
-    o3d.visualization.draw_geometries_with_editing([point_cloud])
-
-if __name__ == "__main__":
-    point_cloud = o3d.geometry.PointCloud()  # Load or create a point cloud
-    demo_crop_geometry(point_cloud)
